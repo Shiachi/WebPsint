4424b9e5fd68c8c6dc2476f4f614e91921bffacf
CristobalAlonso <cristobal.pavez.bravo@gmail.com>
2022-10-22 16:35:21 -0300

    intro changes

--- views/introduction/Intro.php
+++ views/introduction/Intro.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pseint introduccion</title>
+    <title>Document</title>
 </head>
 <body>
-    <h1>Introduccion Pseint</h1>
-    <br>
-    <h3>Paso 1</h3>
-    <p>Para comenzar descarga la app del siguiente <a href="https://pseint.sourceforge.net/?page=descargas.php">link</a><p>
-    <br>
-    
+    <h1>Introduccion a Pseint</h1>
+    <h3>¿Que es PseInt?</h5>
+    <p>La herramienta PSeInt es un intérprete de un lenguaje
+        de programación basado en pseudocódigo. El pseudocódigo 
+        es una descripción informal de alto nivel de un algoritmo, 
+        que manteniendo las convenciones estructurales de un lenguaje 
+        de programación está diseñado para la lectura humana<p>
+    <h3>Para comenzar ve este video introductorio</h3>
+    <iframe width="1280" height="720" src="https://www.youtube.com/embed/FvibfpSVFBw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </body>
 </html>
