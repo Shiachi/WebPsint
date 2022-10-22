9acfe9b5a7f2ca47f7123261b9d4577cc4d4ff2b
CristobalAlonso <cristobal.pavez.bravo@gmail.com>
2022-10-22 14:17:25 -0300

    folders structure and introduction

---
+++ views/introduction/Intro.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pseint introduccion</title>
+</head>
+<body>
+    <h1>Introduccion Pseint</h1>
+    <br>
+    <h3>Paso 1</h3>
+    <p>Para comenzar descarga la app del siguiente <a href="https://pseint.sourceforge.net/?page=descargas.php">link</a><p>
+    <br>
+    
+</body>
+</html>
