9fb8010b7b75686b30240a4748767f7fea3e108a
Shiachi <80282335+Shiachi@users.noreply.github.com>
2022-10-23 16:16:50 -0300

    tutoriales 1,2,3 DevShiachi

---
+++ views/tutorial/tutorial_5.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variables pseint</title>
+</head>
+<body>
+<div class="wrapper">
+        <h1>Variables</h1>
+        <p>Una variable es un espacio de almacenamiento reservado en el dispositivo el cual contiene datos, se llamaanvariables porque estos datos se pueden modificar</p>
+    </div>
+</body>
+</html>
