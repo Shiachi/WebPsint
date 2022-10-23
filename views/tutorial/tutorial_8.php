0eebb5c1081569b7b0689258a3a3ae8b29441077
CristobalAlonso <cristobal.pavez.bravo@gmail.com>
2022-10-23 16:58:34 -0300

    style changes and new page

---
+++ views/tutorial/tutorial_8.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variables pseint</title>
+</head>
+<body>
+<div class="wrapper">
+        <h1>Comando Si - Entonces</h1>
+        <p>Este comando nos permite comparar con expresiones logicas y tener un resultado especifico</p>
+        <h2>Desafio</h2>
+        <p>Crea dos variables de tipo numerica con el nombre
+        que tu decidas, la primera variable debe tener un valor de 8
+        y la segunda un valor de 16 y deberas motrar por consola el numero mayor
+        </p>
+        <p>respuesta consola= 16</p>
+    </div>
+</body>
+</html>
