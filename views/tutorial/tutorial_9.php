c2dce032329459201d932c01dbb617c286138447
CristobalAlonso <cristobal.pavez.bravo@gmail.com>
2022-10-23 21:16:29 -0300

    new tutorials

---
+++ views/tutorial/tutorial_9.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variables pseint</title>
+</head>
+<body>
+<div class="wrapper">
+        <h1>Comando Segun</h1>
+        <p>Este comando nos permite ejecutar una accion segun la variable ingresada</p>
+        <h2>Desafio</h2>
+        <p>
+            Crea una variable de tipo numerico entre 1 y 12 y que la consola
+            devuelva el mes correspondiente
+        </p>
+        <p>
+            Ejemplo: si el numero ingresado es 1 el dia mostrado en consola seria Lunes
+        </p>
+        <p id="response">Posible respuesta variable = 1 consola muestra enero</p>
+        <p id="response">Posible respuesta variable = 2 consola muestra febrero</p>
+        <p id="response">Posible respuesta variable = 3 consola muestra marzo</p>
+        <p id="response">Posible respuesta variable = 4 consola muestra abril</p>
+        <p id="response">Posible respuesta variable = 5 consola muestra mayo</p>
+        <p id="response">Posible respuesta variable = 6 consola muestra junio</p>
+        <p id="response">Posible respuesta variable = 7 consola muestra julio</p>
+        <p id="response">Posible respuesta variable = 8 consola muestra agosto</p>
+        <p id="response">Posible respuesta variable = 9 consola muestra septiembre</p>
+        <p id="response">Posible respuesta variable = 10 consola muestra octubre</p>
+        <p id="response">Posible respuesta variable = 11 consola muestra noviembre</p>
+        <p id="response">Posible respuesta variable = 12 consola muestra dicimebre</p>
+    </div>
+</body>
+</html>
